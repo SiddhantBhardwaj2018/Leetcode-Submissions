811f9f16bede03ebadd1a3dfa91b956c1fcb579c
Siddhant Bhardwaj <44534839+SiddhantBhardwaj2018@users.noreply.github.com>
2019-12-25 21:59:22 -0700

    Add files via upload

---
+++ Pow(x,n).py
@@ -0,0 +1,11 @@
+
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        '''
+        Problem Description - Implement pow(x, n), which calculates x raised to the power n (xn).
+        
+        Performance - Runtime: 28 ms, faster than 83.71% of Python3 online submissions for Pow(x, n).
+        
+        Memory Usage: 12.7 MB, less than 100.00% of Python3 online submissions for Pow(x, n).
+        '''
+        return x ** n
